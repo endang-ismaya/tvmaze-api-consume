72afa9b53567d10c0237b4cafb6fc4e12dff94f6
endang-ismaya <endang.ismaya@gmail.com>
2020-05-03 08:11:35 -0700

    5/3/2020: style signin form

--- pages/signin.js
+++ pages/signin.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import axios from 'axios';
+import cookies from 'nookies';
 import CustomInput from './../components/customInput/CustomInput';
+import { useRouter } from 'next/router';
 
 const initialState = {
   email: '',
@@ -9,16 +11,25 @@ const initialState = {
 
 const SignIn = () => {
   const [signinInfo, setSigninInfo] = useState(initialState);
+  const router = useRouter();
 
   const handleSubmit = async e => {
     e.preventDefault();
 
-    const res = await axios.post(
-      'https://iwallet-api.herokuapp.com/api/auth/signin',
-      { ...signinInfo }
-    );
+    try {
+      const res = await axios.post(
+        'https://iwallet-api.herokuapp.com/api/auth/signin',
+        { ...signinInfo }
+      );
 
-    console.log(res);
+      cookies.set(null, 'token', res.data.token, {
+        path: '/',
+      });
+
+      router.replace('/[country]', '/us');
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handleChange = e => {
@@ -30,6 +41,7 @@ const SignIn = () => {
 
   return (
     <div className="signin">
+      <h3>Sign In</h3>
       <form onSubmit={handleSubmit}>
         <CustomInput
           name="email"
