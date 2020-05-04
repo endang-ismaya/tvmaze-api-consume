5412aa2914b22c5a0abe851c1915d1cec160d65b
endang-ismaya <endang.ismaya@gmail.com>
2020-05-04 03:22:06 -0700

    5/4/2020: initial express.js

--- components/cast/Cast.js
+++ components/cast/Cast.js
@@ -3,13 +3,15 @@ import Thumbnail from './../thumbnail/Thumbnail';
 const Cast = ({ cast }) => {
   const renderCast = () => {
     return cast.map((castItem, idx) => {
-      const { image, name } = castItem.person;
+      const { image, name, id } = castItem.person;
 
       return (
         <li key={idx}>
           <Thumbnail
             imageUrl={image ? image.medium : undefined}
             caption={name}
+            href={`/cast?personId=${id}`}
+            as={`/cast/${id}`}
           />
         </li>
       );
